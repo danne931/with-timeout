3312e91cc01ed39a981e37ab8f2871c98627b04f
Daniel Eisenbarger <danne9.31@gmail.com>
2016-07-15 19:04:46 -0700

    move file

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-function withTimeout (fn, { fnArgs, time = 500 } = {}) {
+module.exports = function timeout (fn, { fnArgs, time = 500 } = {}) {
   return new Promise((resolve, reject) => {
     const timeout = setTimeout(() => {
       clearTimeout(timeout)
       resolve(_.isFunction(fn) ? fn(fnArgs) : undefined)
     }, time)
   })
-}
-
-module.exports = {
-  withTimeout
 }
