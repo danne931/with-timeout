ad4501cb480bd1784e9c556a22569bd3c8588e81
Daniel Eisenbarger <danne9.31@gmail.com>
2016-07-15 19:06:36 -0700

    remove lodash dep

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports = function timeout (fn, { fnArgs, time = 500 } = {}) {
+export default function timeout (fn, { fnArgs, time = 500 } = {}) {
   return new Promise((resolve, reject) => {
     const timeout = setTimeout(() => {
       clearTimeout(timeout)
-      resolve(_.isFunction(fn) ? fn(fnArgs) : undefined)
+      resolve(typeof fn === 'function' ? fn(fnArgs) : undefined)
     }, time)
   })
 }
